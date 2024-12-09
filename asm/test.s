63fceab06aaa439b7524187c36cfecfaaf5e4b12
Michael Shaw <spencershawiscool@gmail.com>
2024-12-09 15:49:36 -0500

    test.s: update to test more cases
    This version also loops infinitely if starting at IP=0

--- asm/test.s
+++ asm/test.s
@@ -1,16 +1,19 @@
 # SPDX-License-Identifier: 0BSD
 
-	CMP %A, %A
-test_label:
-	CMP %A, %B
-	JNZ test_label2
-	LD %A, 0
-	JNZ test_label3
-	ST %A, %B
-test_label2:
-	LD %B, 0
-	JNZ test_label
-	ST %A, %B
-test_label3:
-	XOR %A, %A
-	JNZ test_label
+init:
+	ld %a, 1
+	jnz init_end
+init_mid:
+	cmp %a, %a
+	cmp %a, %a
+	ld %b, 1
+	jnz init_mid
+init_end:
+	cmp %c, %d
+	xor %d, %a
+	ld %a, 30
+	or %b, %a
+	ld %a, 1
+	jnz init_mid
+
+	
